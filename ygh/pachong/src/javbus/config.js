f75d872fa2908226ac9c63292bcb9ba428fe7742
guahunyo <2736859895@qq.com>
2021-08-08 16:09:30 +0800

    feat: 加上爬磁链

--- ygh/pachong/src/javbus/config.js
+++ ygh/pachong/src/javbus/config.js
@@ -4,24 +4,18 @@ const COOKIE = ''
 const PROXY = 'http://127.0.0.1:21882'
 // 域名
 const DOMAIN = 'https://www.javbus.com'
-const DOMAIN2 = 'https://btsow.one/'
+const DOMAIN2 = 'https://btsow.one'
 // 搜索关键词
-const USERNAME = '明里つむぎ'
-// 同一时间发起的请求数量
-const THREAD_COUNT = 3
-// 搜索第一页的参数拼接
-const FIRST_PART_API = `${DOMAIN}/search/${encodeURI(USERNAME)}`
-// 固定值，一般不用修改
-const OTHER_PART_API = `${DOMAIN2}/graphql/query/`
-// 固定值，一般不用修改
-const HOMEPAGE_HASH = 'bfa387b2992c3a52dcbe447467b4b771'
+const SEARCHTEXT = '明里つむぎ'
+// 搜索番号的参数拼接
+const FANHAO_API = `${DOMAIN}/search/${encodeURI(SEARCHTEXT)}`
+// 搜索番号的参数拼接
+const BT_API = `${DOMAIN2}/search`
 
 module.exports = {
-    COOKIE,
-    PROXY,
-    USERNAME,
-    THREAD_COUNT,
-    FIRST_PART_API,
-    OTHER_PART_API,
-    HOMEPAGE_HASH,
+  COOKIE,
+  PROXY,
+  SEARCHTEXT,
+  FANHAO_API,
+  BT_API,
 }
--- ygh/pachong/src/javbus/index.js
+++ ygh/pachong/src/javbus/index.js
@@ -1,12 +1,8 @@
-const fs = require('fs')
-const path = require('path')
-const chalk = require('chalk')
-const async = require('async')
 const cheerio = require('cheerio')
 const request = require('superagent')
 require('superagent-proxy')(request)
 
-const { COOKIE, PROXY, USERNAME, THREAD_COUNT, FIRST_PART_API, OTHER_PART_API, HOMEPAGE_HASH } = require('./config.js')
+const { COOKIE, PROXY, FANHAO_API, BT_API } = require('./config.js')
 
 class Crawler {
   constructor() {
@@ -16,19 +12,39 @@ class Crawler {
   }
 
   init() {
-    this.fetchFirstPageData()
+    this.fetchData()
   }
 
   /**
    * 获取第一页的番号数据
    */
-  async fetchFirstPageData() {
-    console.log(FIRST_PART_API)
+  async fetchData() {
+    // console.log(FANHAO_API)
     try {
       let page = 1;
       let results = [];
+      // const fetchbt = async (search) => {
+      //   const res = await request.get(`${BT_API}/${search}`).set({ cookie: COOKIE }).proxy(PROXY)
+      //   const $ = cheerio.load(res.text)
+      //   console.log('111', res)
+      // }
+      const fnbt = async (results) => {
+        let current = 0;
+        let len = results.length;
+        while(current < len) {
+          const res = await request.get(`https://sukebei.nyaa.si/?q=${results[current].id}`).set({ cookie: COOKIE }).proxy(PROXY)
+          const $ = cheerio.load(res.text)
+          $('.default').each(function (i, elem) {
+            results[current].bt.push($(this).children('.text-center').find('a').next().attr('href').split('&dn=')[0])
+          });
+          current++
+          if (current === len) {
+            console.log('磁力爬取完毕', results)
+          }
+        }
+      }
       const fn = async () => {
-        const res = await request.get(`${FIRST_PART_API}/${page}`).set({ cookie: COOKIE }).proxy(PROXY)
+        const res = await request.get(`${FANHAO_API}/${page}`).set({ cookie: COOKIE }).proxy(PROXY)
         const $ = cheerio.load(res.text)
 
         $('.photo-info').each(function (i, elem) {
@@ -44,118 +60,14 @@ class Crawler {
           page++;
           fn();
         } else {
-          console.log('一共', results.length, '个结果：', results)
+          console.log('一共', results.length, '个番号， 开始爬取磁力。。。')
           fnbt(results)
         }
-      }
-      const fnbt = async () => {
-        const res = await request.get(`${FIRST_PART_API}/${page}`).set({ cookie: COOKIE }).proxy(PROXY)
-        const $ = cheerio.load(res.text)
-
       }
       fn();
       
     } catch (err) {
       console.error('抓取第一页数据失败：', err)
-    }
-  }
-
-  /**
-   * 抓取用户其他页图片数据
-   * @param {string} id 用户id
-   * @param {string} after 下一页的标识符
-   */
-  async fetchOtherPageData(id, after) {
-    const query = {
-      query_hash: HOMEPAGE_HASH,
-      variables: `{"id":"${id}","first":12,"after":"${after}"}`,
-    }
-    try {
-      const res = await request
-        .get(OTHER_PART_API)
-        .query(query)
-        .set({ 'Content-Type': 'application/json', cookie: COOKIE })
-        .proxy(PROXY)
-      const {
-        data: { user },
-      } = res.body
-      const userinfo = user.edge_owner_to_timeline_media
-      const { count, page_info, edges } = userinfo
-      await this.parseSource(edges)
-
-      if (page_info.has_next_page) {
-        await this.fetchOtherPageData(id, page_info.end_cursor)
-      }
-    } catch (err) {
-      console.error('抓取用户其他页数据失败：', err.message)
-    }
-  }
-
-  /**
-   * 解析图片地址并进行下载
-   * @param {array} edges 用户主页信息
-   */
-  async parseSource(edges) {
-    return async.mapLimit(edges, THREAD_COUNT, async ({ node }) => {
-      if (node.is_video) {
-        return await this.downloadVideo(node.video_url)
-      } else {
-        return await this.downloadImage(node.display_url)
-      }
-    })
-  }
-
-  /**
-   * 下载图片到本地
-   * @param {string} url 图片地址
-   */
-  async downloadImage(url) {
-    const userPath = path.resolve(__dirname, 'assets', USERNAME)
-    if (!fs.existsSync(userPath)) {
-      fs.mkdirSync(userPath)
-    }
-    const pattern = /[^\/]+\.jpg/
-    const fileName = url.match(pattern)[0]
-    const filePath = path.resolve(userPath, fileName)
-
-    try {
-      if (!fs.existsSync(filePath)) {
-        const res = await request.get(url).proxy(PROXY)
-        fs.writeFileSync(filePath, res.body)
-        console.log(chalk.hex('#FF9966')(`图片${fileName}下载成功`))
-      } else {
-        console.log(chalk.hex('#FF9966')(`图片${fileName}已经存在`))
-      }
-      return fileName
-    } catch (err) {
-      console.error('图片下载失败：', err.message)
-    }
-  }
-
-  /**
-   * 下载视频到本地
-   * @param {string} url 视频地址
-   */
-  async downloadVideo(url) {
-    const userPath = path.resolve(__dirname, 'assets', USERNAME)
-    if (!fs.existsSync(userPath)) {
-      fs.mkdirSync(userPath)
-    }
-    const pattern = /[^\/]+\.mp4/
-    const fileName = url.match(pattern)[0]
-    const filePath = path.resolve(userPath, fileName)
-
-    try {
-      if (!fs.existsSync(filePath)) {
-        const res = await request.get(url).proxy(PROXY)
-        fs.writeFileSync(filePath, res.body)
-        console.log(chalk.hex('#FFCCCC')(`视频${fileName}下载成功`))
-      } else {
-        console.log(chalk.hex('#FFCCCC')(`视频${fileName}已经存在`))
-      }
-      return fileName
-    } catch (err) {
-      console.error('视频下载失败：', err.message)
     }
   }
 }
